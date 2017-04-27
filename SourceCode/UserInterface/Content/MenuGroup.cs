617a480818ba645b1e6678196752394163c9ebc0
LeeKhai <leekhai18@gmail.com>
2017-04-27 12:35:28 +0700

    Use modernUI and update project

---
+++ SourceCode/UserInterface/Content/MenuGroup.cs
@@ -0,0 +1,23 @@
+﻿using FirstFloor.ModernUI.Windows.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstFloor.ModernUI.App.Content
+{
+    public class MenuGroup
+    {
+        public static void Add(ModernWindow modernWindow, string groupName, string linkName, String source)
+        {
+            modernWindow.MenuLinkGroups.Add(new FirstFloor.ModernUI.Presentation.LinkGroup() { DisplayName = groupName, Links = { new FirstFloor.ModernUI.Presentation.Link() { DisplayName = linkName, Source = new Uri(source, UriKind.Relative) } } });
+        }
+
+        public static void Remove(ModernWindow modernWindow, int index, String contentSource)
+        {
+            modernWindow.MenuLinkGroups.RemoveAt(index);
+            modernWindow.ContentSource = new Uri(contentSource, UriKind.Relative);
+        }
+    }
+}
